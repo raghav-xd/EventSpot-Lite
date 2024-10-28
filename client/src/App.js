451f266fb77b3c9e5ce851e42e121d126c2584d1
raghav-xd <102276584+raghav-xd@users.noreply.github.com>
2024-10-28 19:19:15 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import EventList from "./components/EventList";
 import EventModal from "./components/EventModal";
