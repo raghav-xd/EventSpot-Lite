fa05cde018763bcc7fc8dc54bcb6e6d8212a75b6
raghav-xd <102276584+raghav-xd@users.noreply.github.com>
2024-10-28 23:25:36 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
 import EventList from "./components/EventList";
 import EventModal from "./components/EventModal";
 import eventsData from "./Data";
 import "./App.css";
-
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -18,19 +15,28 @@ function App() {
       event.location.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  function toggleMenu() {
+    const navLinks = document.querySelector('.navbar-links');
+    if (navLinks.style.display !== "none") {
+      navLinks.style.display = "none";
+    } else {
+      navLinks.style.display = "flex";
+    }
+  }
+
   return (
     <div className="App">
       <header className="navbar">
         <div className="navbar-left">
           <h1>EventSpot Lite</h1>
-          <p style={{color:"rgb(69, 67, 67)"}}>Welcome!!</p>
+          <p style={{ color: "rgb(69, 67, 67)" }}>Welcome!!</p>
         </div>
         <nav className="navbar-links">
           <button onClick={() => setPage("home")}>Home</button>
           <button onClick={() => setPage("trending")}>Trending</button>
           <button onClick={() => setPage("schedule")}>Schedule</button>
         </nav>
-
+        <div className="menu-icon" onClick={toggleMenu}>☰</div>
       </header>
       <div id="search-container">
         <input
@@ -40,15 +46,12 @@ function App() {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
-
       {page === "home" && (
         <EventList events={filteredEvents} onEventClick={setSelectedEvent} />
       )}
       {selectedEvent && (
         <EventModal event={selectedEvent} onClose={() => setSelectedEvent(null)} />
       )}
-
-
     </div>
   );
 }
